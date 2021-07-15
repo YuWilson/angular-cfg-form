5bf8bd868b49dc7a624173b5b89b18c21097458e
YuWilson <wilsonyu.pg@gmail.com>
2021-07-15 15:10:40 +0800

    1 file modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import {
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
